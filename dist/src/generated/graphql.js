128f3e046435da99f885f76d2f2ab9d3f4ffcf49
OleksandrS <sai7602@ukr.net>
2022-07-27 16:46:28 +0300

    add dist

---
+++ dist/src/generated/graphql.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AnimeType = exports.AiringStatus = void 0;
+var AiringStatus;
+(function (AiringStatus) {
+    AiringStatus["Airing"] = "AIRING";
+    AiringStatus["Complete"] = "COMPLETE";
+    AiringStatus["Upcoming"] = "UPCOMING";
+})(AiringStatus = exports.AiringStatus || (exports.AiringStatus = {}));
+var AnimeType;
+(function (AnimeType) {
+    AnimeType["Movie"] = "MOVIE";
+    AnimeType["Music"] = "MUSIC";
+    AnimeType["Ona"] = "ONA";
+    AnimeType["Ova"] = "OVA";
+    AnimeType["Special"] = "SPECIAL";
+    AnimeType["Tv"] = "TV";
+})(AnimeType = exports.AnimeType || (exports.AnimeType = {}));
+//# sourceMappingURL=graphql.js.map
